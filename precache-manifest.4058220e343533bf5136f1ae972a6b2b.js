6371788bccb23ccb9ddeb18ae7cecc79090b3a37
bjsmith3185 <bjsmith3185@hotmail.com>
2018-11-19 17:36:12 -0500

    Updates

---
+++ precache-manifest.4058220e343533bf5136f1ae972a6b2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d836daae59335bedebb",
+    "url": "/React_guessing_game/static/js/runtime~main.2d836daa.js"
+  },
+  {
+    "revision": "5d617c6b3a0ff76e5249",
+    "url": "/React_guessing_game/static/js/main.5d617c6b.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/React_guessing_game/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "5d617c6b3a0ff76e5249",
+    "url": "/React_guessing_game/static/css/main.2938b753.chunk.css"
+  },
+  {
+    "revision": "dbc2edd4d6fd74a64a89ce101c70803e",
+    "url": "/React_guessing_game/index.html"
+  }
+];
